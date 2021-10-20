b0a8911e40660fbfff5ecffaab6460ba8e895f87
Laura Buffington <laura.c.buffington@gmail.com>
2021-10-19 23:21:40 -0700

    countPrimes

---
+++ countPrimes.js
@@ -0,0 +1,29 @@
+function countPrimes(n){
+    if(n <= 2) return 0;
+
+    //first, create array at same length as n with all elements being true
+    let arr = [];
+    for(let i = 0; i < n; i++){
+        arr.push(true)
+    }
+    //implement Sieve of Eratosthenes algo
+    //where at each iteration, 
+    //if element is true, flip all multiples of that coinciding number to false.
+    //all initially start as true from arr
+    for(let i = 2; i <= Math.floor(Math.sqrt(n)); i++){
+        if(arr[i]){
+            for(let j = i; j*i < arr.length; j++){
+                arr[i*j] = false
+            }
+        }
+    }
+    //now count the remaining elements that equal true
+    let count = 0;
+    for(let i = 2; i < arr.length; i++){
+        if(arr[i]){
+            count++
+        }
+    }
+    return count
+
+}
